03b6b63c6228e0809e3a3ee99f98193f0da333b4
NathanD19 <nathandavies19@yahoo.com.au>
2019-08-17 01:00:48 +1000

    Added Skills Images. Added Custom tooltip using AWESOME Tippy.js

---
+++ assets/js/tooltips.js
@@ -0,0 +1,19 @@
+tippy('svg', {
+    arrow: true,
+    arrowType: 'round',
+    animateFill: false,
+    animation: 'shift-away',
+    placement: 'bottom',
+    theme: 'translucent',
+    size: 'large',
+});
+
+tippy('#sqlservertooltip', {
+    arrow: true,
+    arrowType: 'round',
+    animateFill: false,
+    animation: 'shift-away',
+    placement: 'bottom',
+    theme: 'translucent',
+    size: 'large',
+});
